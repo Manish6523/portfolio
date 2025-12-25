3b604297a417cdb8abeb5dea71e9888136198967
Manish6523 <220101005092@gandhinagaruni.ac.in>
2025-12-25 22:43:35 +0530

    added padding

--- components/PageTransition.tsx
+++ components/PageTransition.tsx
@@ -40,7 +40,7 @@ export default function PageTransition({ children }: PageTransitionProps) {
         animate="animate"
         exit="exit"
         variants={pageVariants}
-        transition={pageTransition}
+        // transition={pageTransition}
       >
         {children}
       </motion.div>
